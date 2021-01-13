9e897c9758249fa2582bf75994d3a245766cb7cb
JasonWilger <jkw500@gmail.com>
2021-01-13 15:42:24 -0500

    fixed pathing issue for photo

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const path = require("path");
 
 // use the express-static middleware
 app.use(express.static("public"))
 app.use(express.static("views"))
 
-// define the first route
-app.get('/', function (req, res,html) {
-    res.sendFile(path.join(__dirname+'/photo.html'));
-   });
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
